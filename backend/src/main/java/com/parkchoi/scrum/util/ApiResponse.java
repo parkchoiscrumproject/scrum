605478ed8496a3b4f6bb4a1456b549e7865ec2fe
qkrrlgus114 <qkrrlgus114@naver.com>
2023-12-15 23:39:55 +0900

    Feat : 응답 api 추가

---
+++ backend/src/main/java/com/parkchoi/scrum/util/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.parkchoi.scrum.util;
+
+public class ApiResponse<T> {
+
+    // 상태 구분
+    private static final String SUCCESS_STATUS = "success";
+    private static final String FAIL_STATUS = "fail";
+    private static final String ERROR_STATUS = "error";
+
+    private String status;
+    private T data;
+    private String message;
+
+    public ApiResponse(String status, T data, String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    // 성공했을 때 data와 message 리턴
+    public static <T> ApiResponse<T> createSuccess(T data, String message){
+        return new ApiResponse<>(SUCCESS_STATUS, data, message);
+    }
+
+    // 성공했지만 리턴 데이터가 없는 경우 message 리턴
+    public static <T> ApiResponse<T> createSuccessNoContent(String message){
+        return new ApiResponse<>(SUCCESS_STATUS, null, message);
+    }
+
+    // 예외 발생으로 API 호출 실패시 반환
+    public static ApiResponse<?> createError(String message) {
+        return new ApiResponse<>(ERROR_STATUS, null, message);
+    }
+
+
+}
