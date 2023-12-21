5b341d7a068734c851742d056b42570fd5c98761
박기현 <qkrrlgus114@naver.com>
2023-12-21 23:13:43 +0900

    Feat : 스웨거 추가

--- backend/src/main/java/com/parkchoi/scrum/domain/user/controller/UserController.java
+++ backend/src/main/java/com/parkchoi/scrum/domain/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.parkchoi.scrum.domain.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @GetMapping("/")
