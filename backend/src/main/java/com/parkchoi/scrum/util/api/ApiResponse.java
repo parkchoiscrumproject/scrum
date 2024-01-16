0250c60c484892713d0a204e30f4a7dbfdd5239f
Eungae-D <s4078942@naver.com>
2024-01-16 15:40:17 +0900

    Fix : 에러 response 변경

--- backend/src/main/java/com/parkchoi/scrum/util/api/ApiResponse.java
+++ backend/src/main/java/com/parkchoi/scrum/util/api/ApiResponse.java
@@ -33,8 +33,13 @@ public class ApiResponse<T> {
         return new ApiResponse<>(SUCCESS_STATUS, null, message);
     }
 
-    // 예외 발생으로 API 호출 실패시 반환
-    public static ApiResponse<?> createError(String message) {
+    // 예외 발생으로 API 호출 실패시 반환 (클라이언트 에러)
+    public static ApiResponse<?> createClientError(String message) {
+        return new ApiResponse<>(FAIL_STATUS, null, message);
+    }
+
+    // 예외 발생으로 API 호출 실패시 반환 (서버 에러)
+    public static ApiResponse<?> createServerError(String message) {
         return new ApiResponse<>(ERROR_STATUS, null, message);
     }
 
