460878a6fe2f0b7b478529f6d6c7cd29337dd9ee
qkrrlgus114 <qkrrlgus114@naver.com>
2024-01-18 18:28:10 +0900

    Feat : 레포지토리 테스트 코드 작성중

--- backend/src/test/java/com/parkchoi/scrum/domain/user/UserControllerTest.java
+++ backend/src/test/java/com/parkchoi/scrum/domain/user/UserControllerTest.java
@@ -1,6 +1,9 @@
 package com.parkchoi.scrum.domain.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkchoi.scrum.domain.user.controller.UserController;
+import com.parkchoi.scrum.domain.user.dto.response.UserLoginInfoResponseDTO;
+import com.parkchoi.scrum.domain.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.mock.web.MockCookie;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,35 +24,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 // 특정 클래스의 빈만 로드하기 위해서 사용
 @WebMvcTest(UserControllerTest.class)
-@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
-    private ObjectMapper objectMapper;
-
+    @MockBean
+    private UserService userService;
 
     // 테스트 전에 MockMvc 인스턴스 초기화
     @BeforeEach
     public void setup(){
-        mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .addFilter(new CharacterEncodingFilter("UTF-8, true"))
-                .alwaysDo(print())
-                .build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new UserController(userService)).build();
     }
 
     @Test
     @DisplayName("유저 로그인 api 테스트")
     public void 유저_로그인() throws Exception{
 
-        MockCookie cookie = new MockCookie("accessToken", "테스트용 토큰");
+        String accessToken = "asdasd";
+        UserLoginInfoResponseDTO mockResponse = new UserLoginInfoResponseDTO();
 
-        mockMvc.perform(post("/api/user/login")
-                .cookie(cookie));
+
 
     }
 }
