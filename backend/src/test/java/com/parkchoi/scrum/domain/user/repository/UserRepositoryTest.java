a4c4a129c84dd069d7f132b554cbdcd2f66222a8
박기현 <qkrrlgus114@naver.com>
2024-01-18 20:11:27 +0900

    Feat : 유저 리포지토리 테스트코드 완성

---
+++ backend/src/test/java/com/parkchoi/scrum/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.parkchoi.scrum.domain.user.repository;
+
+import com.parkchoi.scrum.domain.user.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        User user = User.builder()
+                .email("test@test.com")
+                .profileImage("ex")
+                .nickname("ex")
+                .type("kakao").build();
+        userRepository.save(user);
+    }
+
+    @Test
+    @DisplayName("이메일로 회원 검색")
+    void 이메일로_회원_찾기(){
+        // given
+        String email = "test@test.com";
+        // when
+        Optional<User> byEmail = userRepository.findByEmail(email);
+
+        // then
+        Assertions.assertTrue(byEmail.isPresent());
+        Assertions.assertEquals(byEmail.get().getEmail(), email);
+    }
+
+    @Test
+    @DisplayName("닉네임 존재 여부 테스트(존재함)")
+    void 중복된_닉네임_존재(){
+        // given
+        String nickname = "ex";
+
+        // when
+        Boolean result = userRepository.existsByNickname(nickname);
+
+        // then
+        Assertions.assertEquals(result, true);
+    }
+
+    @Test
+    @DisplayName("닉네임 존재 여부 테스트(존재하지 않음)")
+    void 중복된_닉네임_존재하지_않음(){
+        // given
+        String nickname = "sad";
+
+        // when
+        Boolean result = userRepository.existsByNickname(nickname);
+
+        // then
+        Assertions.assertEquals(result, false);
+    }
+
+}
