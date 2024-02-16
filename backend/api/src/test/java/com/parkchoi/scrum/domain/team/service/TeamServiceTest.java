955d69542301141dd690668a01bccc2393c93260
Eungae-D <s4078942@naver.com>
2024-02-16 19:48:04 +0900

    Feat : TeamServiceTest 팀 생성 성공 테스트

--- backend/api/src/test/java/com/parkchoi/scrum/domain/team/service/TeamServiceTest.java
+++ backend/api/src/test/java/com/parkchoi/scrum/domain/team/service/TeamServiceTest.java
@@ -2,25 +2,32 @@ package com.parkchoi.scrum.domain.team.service;
 
 import com.parkchoi.scrum.domain.team.dto.request.CreateTeamRequestDTO;
 import com.parkchoi.scrum.domain.team.dto.request.TeamInfoDTO;
+import com.parkchoi.scrum.domain.team.dto.response.CreateTeamResponseDTO;
 import com.parkchoi.scrum.domain.team.entity.Team;
 import com.parkchoi.scrum.domain.team.repository.TeamRepository;
 import com.parkchoi.scrum.domain.user.entity.User;
 import com.parkchoi.scrum.domain.user.repository.UserRepository;
 import com.parkchoi.scrum.util.jwt.JwtUtil;
 import com.parkchoi.scrum.util.s3.S3UploadService;
+import org.apache.juli.logging.Log;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @ExtendWith(MockitoExtension.class)
 public class TeamServiceTest {
@@ -68,9 +75,10 @@ public class TeamServiceTest {
 
 
     @Test
-    void 팀_생성_성공(){
+    void 팀_생성_성공()throws IOException{
         Mockito.when(jwtUtil.getUserId(accessToken)).thenReturn(userId);
         Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
+
 
         //팀 정보 생성
         TeamInfoDTO teamInfo = TeamInfoDTO.builder()
@@ -87,6 +95,14 @@ public class TeamServiceTest {
         MockMultipartFile file = new MockMultipartFile("file","test.png","image/png","testImageContent".getBytes());
 
 
+        //when
+        CreateTeamResponseDTO result = teamService.createTeam(accessToken, file, createTeamRequestDTO);
+
+        //then
+        Mockito.verify(s3UploadService).saveFile(Mockito.any(MultipartFile.class));
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("팀이름", result.getName());
+        Assertions.assertEquals("image_url", result.getImageUrl());
 
 
     }
