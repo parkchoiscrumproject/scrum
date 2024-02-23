79880924a129c33c4ca8c3f7cdc00286bf94d24b
박기현 <qkrrlgus114@naver.com>
2024-02-24 02:01:32 +0900

    Feat : 스크럼 조회 stream 적용

--- backend/api/src/main/java/com/parkchoi/scrum/domain/scrum/service/impl/ScrumServiceImpl.java
+++ backend/api/src/main/java/com/parkchoi/scrum/domain/scrum/service/impl/ScrumServiceImpl.java
@@ -1,17 +1,15 @@
 package com.parkchoi.scrum.domain.scrum.service.impl;
 
 import com.parkchoi.scrum.domain.exception.exception.ScrumException;
-import com.parkchoi.scrum.domain.exception.exception.UserException;
 import com.parkchoi.scrum.domain.exception.info.ScrumExceptionInfo;
-import com.parkchoi.scrum.domain.exception.info.UserExceptionInfo;
 import com.parkchoi.scrum.domain.scrum.dto.request.CreateScrumRequestDTO;
 import com.parkchoi.scrum.domain.scrum.dto.response.ScrumPageResponseDTO;
 import com.parkchoi.scrum.domain.scrum.dto.response.ScrumRoomDTO;
 import com.parkchoi.scrum.domain.scrum.dto.response.ScrumRoomListResponseDTO;
 import com.parkchoi.scrum.domain.scrum.entity.Scrum;
 import com.parkchoi.scrum.domain.scrum.entity.ScrumParticipant;
-import com.parkchoi.scrum.domain.scrum.repository.scrumparticipant.ScrumParticipantRepository;
 import com.parkchoi.scrum.domain.scrum.repository.scrum.ScrumRepository;
+import com.parkchoi.scrum.domain.scrum.repository.scrumparticipant.ScrumParticipantRepository;
 import com.parkchoi.scrum.domain.scrum.service.ScrumService;
 import com.parkchoi.scrum.domain.team.entity.Team;
 import com.parkchoi.scrum.domain.team.exception.NonParticipantUserException;
@@ -19,25 +17,22 @@ import com.parkchoi.scrum.domain.team.exception.TeamNotFoundException;
 import com.parkchoi.scrum.domain.team.repository.InviteTeamListRepository;
 import com.parkchoi.scrum.domain.team.repository.TeamRepository;
 import com.parkchoi.scrum.domain.user.entity.User;
-import com.parkchoi.scrum.domain.user.repository.user.UserRepository;
 import com.parkchoi.scrum.util.SecurityContext;
-import com.parkchoi.scrum.util.jwt.JwtUtil;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ScrumServiceImpl implements ScrumService {
 
-    private final UserRepository userRepository;
     private final ScrumRepository scrumRepository;
     private final ScrumParticipantRepository scrumParticipantRepository;
     private final InviteTeamListRepository inviteTeamListRepository;
@@ -72,6 +67,7 @@ public class ScrumServiceImpl implements ScrumService {
 
     // 스크럼 팀 조회
     @Override
+    @Transactional(readOnly = true)
     public ScrumRoomListResponseDTO findScrums(Long teamId) {
         User user = securityContext.getUser();
 
@@ -79,17 +75,13 @@ public class ScrumServiceImpl implements ScrumService {
                 .orElseThrow(() -> new TeamNotFoundException("DB에서 " + teamId + "번 팀을 찾지 못했습니다."));
 
         inviteTeamListRepository.findByUserAndTeamAndParticipantIsTrue(user, team)
-                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + "번 유저가 존재 하지 않습니다."));
+                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + user.getId() + "번 유저가 존재 하지 않습니다."));
 
         List<Scrum> scrumList = scrumRepository.findActiveScrumsByTeam(team);
-        List<ScrumRoomDTO> scrumRoomDTOList = new ArrayList<>();
 
-        for (Scrum s : scrumList) {
-            if (s.getEndTime() == null) {
-                ScrumRoomDTO scrumRoomDTO = ScrumRoomDTO.fromEntity(s);
-                scrumRoomDTOList.add(scrumRoomDTO);
-            }
-        }
+        List<ScrumRoomDTO> scrumRoomDTOList = scrumList.stream()
+                .map(ScrumRoomDTO::fromEntity)
+                .collect(Collectors.toList());
 
         return new ScrumRoomListResponseDTO(scrumRoomDTOList);
     }
@@ -105,10 +97,10 @@ public class ScrumServiceImpl implements ScrumService {
                 .orElseThrow(() -> new TeamNotFoundException("DB에서 " + teamId + "번 팀을 찾지 못했습니다."));
 
         inviteTeamListRepository.findByUserAndTeamAndParticipantIsTrue(user, team)
-                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + "번 유저가 존재 하지 않습니다."));
+                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에 " + user.getId() + "번 유저가 존재 하지 않습니다."));
 
         Scrum scrum = scrumRepository.findActiveScrumByScrumId(scrumId)
-                .orElseThrow(() -> new ScrumException(ScrumExceptionInfo.SCRUM_NOT_FOUND, "DB에서 "+ scrumId+"번 스크럼을 찾지 못했습니다.(삭제 됐거나 종료 됐거나 스크럼이 없음)"));
+                .orElseThrow(() -> new ScrumException(ScrumExceptionInfo.SCRUM_NOT_FOUND, "DB에서 " + scrumId +" 번 스크럼을 찾지 못했습니다.(삭제 됐거나 종료 됐거나 스크럼이 없음)"));
 
         // 아직 참여하지 않은 스크럼이면
         if(!scrumParticipantRepository.existsEnterScrum(user, scrum)){
@@ -124,7 +116,7 @@ public class ScrumServiceImpl implements ScrumService {
             // 현재 인원 증가
             scrum.plusCurrentMember();
         }else{
-            throw new ScrumException(ScrumExceptionInfo.SCRUM_ALREADY_ENTERED, "번 유저는 이미 " + scrumId + "번 스크럼에 참여중 입니다.");
+            throw new ScrumException(ScrumExceptionInfo.SCRUM_ALREADY_ENTERED, user.getId() + "번 유저는 이미 " + scrumId + "번 스크럼에 참여중 입니다.");
         }
         // 나중에 화면에 필요한 정보 리턴
     }
@@ -139,18 +131,15 @@ public class ScrumServiceImpl implements ScrumService {
                 .orElseThrow(() -> new TeamNotFoundException("DB에서 " + teamId + "번 팀을 찾지 못했습니다."));
 
         inviteTeamListRepository.findByUserAndTeamAndParticipantIsTrue(user, team)
-                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" +  "번 유저가 존재 하지 않습니다."));
+                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + user.getId() + "번 유저가 존재 하지 않습니다."));
 
         Scrum scrum = scrumRepository.findActiveScrumByScrumId(scrumId)
                 .orElseThrow(() -> new ScrumException(ScrumExceptionInfo.SCRUM_NOT_FOUND, "DB에서 "+ scrumId+"번 스크럼을 찾지 못했습니다.(삭제 됐거나 종료 됐거나 스크럼이 없음)"));
 
         if (!scrum.getUser().equals(user)) {
-            throw new ScrumException(ScrumExceptionInfo.NOT_SCRUM_LEADER, "번 유저는 " + scrumId + "번 스크럼의 리더가 아니라 삭제 불가능 합니다.");
+            throw new ScrumException(ScrumExceptionInfo.NOT_SCRUM_LEADER, user.getId() + "번 유저는 " + scrumId + "번 스크럼의 리더가 아니라 삭제 불가능 합니다.");
         }
 
-        if (scrum.getDeleteDate() != null){
-            throw new ScrumException(ScrumExceptionInfo.SCRUM_ALREADY_REMOVED, scrumId + "번 스크럼은 이미 삭제된 스크럼 입니다.");
-        }
         // 스크럼 삭제 진행
         scrum.deleteScrum();
     }
@@ -165,7 +154,7 @@ public class ScrumServiceImpl implements ScrumService {
                 .orElseThrow(() -> new TeamNotFoundException("DB에서 " + teamId + "번 팀을 찾지 못했습니다."));
 
         inviteTeamListRepository.findByUserAndTeamAndParticipantIsTrue(user, team)
-                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" +  "번 유저가 존재 하지 않습니다."));
+                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + user.getId() + "번 유저가 존재 하지 않습니다."));
 
         Scrum scrum = scrumRepository.findActiveScrumByScrumId(scrumId)
                 .orElseThrow(() -> new ScrumException(ScrumExceptionInfo.SCRUM_NOT_FOUND, "DB에서 "+ scrumId+"번 스크럼을 찾지 못했습니다.(삭제 됐거나 종료 됐거나 스크럼이 없음)"));
@@ -192,15 +181,10 @@ public class ScrumServiceImpl implements ScrumService {
                 .orElseThrow(() -> new TeamNotFoundException("DB에서 " + teamId + "번 팀을 찾지 못했습니다."));
 
         inviteTeamListRepository.findByUserAndTeamAndParticipantIsTrue(user, team)
-                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + "번 유저가 존재 하지 않습니다."));
+                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + user.getId() + "번 유저가 존재 하지 않습니다."));
 
         Scrum scrum = scrumRepository.findActiveScrumByScrumId(scrumId)
                 .orElseThrow(() -> new ScrumException(ScrumExceptionInfo.SCRUM_NOT_FOUND, "DB에서 "+ scrumId+"번 스크럼을 찾지 못했습니다.(삭제 됐거나 종료 됐거나 스크럼이 없음)"));
-
-        // 이미 삭제된 스크럼이면
-        if (scrum.getDeleteDate() != null){
-            throw new ScrumException(ScrumExceptionInfo.SCRUM_ALREADY_REMOVED, scrumId + "번 스크럼은 이미 삭제된 스크럼 입니다.");
-        }
 
         if (!scrum.getUser().equals(user)) {
             throw new ScrumException(ScrumExceptionInfo.NOT_SCRUM_LEADER, "번 유저는 " + scrumId + "번 스크럼의 리더가 아니라 종료 불가능 합니다.");
@@ -209,11 +193,6 @@ public class ScrumServiceImpl implements ScrumService {
         // 아직 스크럼이 시작하지 않았으면
         if (!scrum.getIsStart()){
             throw new ScrumException(ScrumExceptionInfo.SCRUM_NOT_STARTED, scrumId + "번 스크럼은 아직 시작 하지 않았습니다.");
-        }
-
-        // 이미 스크럼이 종료 상태라면
-        if (scrum.getEndTime() != null){
-            throw new ScrumException(ScrumExceptionInfo.SCRUM_ALREADY_ENDED, scrumId + "번 스크럼은 이미 종료된 스크럼 입니다.");
         }
 
         scrum.endScrum();
@@ -230,13 +209,14 @@ public class ScrumServiceImpl implements ScrumService {
 
     // 스크럼 검색
     @Override
+    @Transactional(readOnly = true)
     public ScrumPageResponseDTO searchScrum(String type, String key, Long teamId, Pageable pageable){
         User user = securityContext.getUser();
         Team team = teamRepository.findById(teamId)
                 .orElseThrow(() -> new TeamNotFoundException("DB에서 " + teamId + "번 팀을 찾지 못했습니다."));
 
         inviteTeamListRepository.findByUserAndTeamAndParticipantIsTrue(user, team)
-                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + "번 유저가 존재 하지 않습니다."));
+                .orElseThrow(() -> new NonParticipantUserException(teamId + "번 팀 초대 리스트에" + user.getId() + "번 유저가 존재 하지 않습니다."));
 
         Page<Scrum> scrums = scrumRepository.searchScrumWithPagination(type, key, pageable);
 
