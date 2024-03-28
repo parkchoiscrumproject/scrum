48710759d29ec9f646d60c16a6a007696b269237
unknown <>
2024-03-28 14:57:54 +0900

    프론트 살짝 변경, material 설치

--- frontend/scrum/src/App.tsx
+++ frontend/scrum/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/HomePage/Home.tsx";
 import Success from "./utils/Success.tsx";
+import Login from "./pages/HomePage/Login.tsx";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/Success" element={<Success />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
--- frontend/scrum/src/pages/HomePage/Home.tsx
+++ frontend/scrum/src/pages/HomePage/Home.tsx
@@ -1,13 +1,9 @@
 import React, { useEffect } from "react";
-import LoginModal from "../../components/common/LoginModal.tsx";
-import logo from "../../assets/logo.png";
-import styles from "../HomePage/Home.module.css";
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <img src={logo} alt="Logo" className={styles.logo} />
-      <LoginModal></LoginModal>
+    <div>
+
     </div>
   );
 };
